efece72afb8a8b2ace5580ae306d5b0582e643e1
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-08-13 16:56:22 +0530

    Update ContactUs.js

--- server/api/ContactUs.js
+++ server/api/ContactUs.js
@@ -3,42 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../model/CustomerMessage');
 
-// Route to handle sending messages
-router.post('/send-message', async (req, res) => {
-  const { name, email, orderId, message, platform } = req.body;
-
-  if (!message) {
-    return res.status(400).json({ message: 'Message content is required.' });
-  }
-
-  if (!platform) {
-    return res.status(402).json({ message: 'Platform is required.' });
-  }
-
-  if (!orderId) {
-    return res.status(401).json({ message: 'Order ID is required.' });
-  }
-
-  try {
-    // Save the message to the database
-    const newMessage = new Message({
-      content: message,
-      name,
-      email,
-      orderId,
-      platform,
-      receiverEmail: 'divyanshuverma36@gmail.com' // Fixed receiver email
-    });
-
-    await newMessage.save();
-    res.status(200).json({ message: 'Message sent successfully!' });
-  } catch (error) {
-    console.error('Error sending message:', error);
-    res.status(500).json({ message: 'Failed to send message.' });
-  }
-});
-
-// server/routes/messageRoutes.js
 
 router.get('/customer_previous_messages', async (req, res) => {
   const { senderEmail } = req.query;
