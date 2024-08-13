551667833d573ec58e552ef758b3f388d0d3ad08
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-08-13 11:10:05 +0530

    Update ContactUs.js

--- server/api/ContactUs.js
+++ server/api/ContactUs.js
@@ -54,7 +54,8 @@ router.get('/customer_previous_messages', async (req, res) => {
     // Query messages where senderEmail matches and receiverEmail is fixed
     const messages = await Message.find({ 
       email: decodedSenderEmail,
-      receiverEmail: "divyanshuverma36@gmail.com"
+      receiverEmail: "divyanshuverma36@gmail.com",
+      closeTicket: false // Exclude messages where closeTicket is true
     });
     
     res.status(200).json(messages);
@@ -63,6 +64,28 @@ router.get('/customer_previous_messages', async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch messages.' });
   }
 });
+
+// handle close ticket route
+
+router.patch('/close_ticket/:id', async (req, res) => {
+  try {
+    const messageId = req.params.id;
+    
+    const message = await Message.findById(messageId);
+    if (!message) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+
+    message.closeTicket = true; // Set closeTicket to true
+    await message.save();
+
+    res.status(200).json({ message: 'Ticket closed successfully' });
+  } catch (error) {
+    console.error('Error closing ticket:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 
 
 module.exports = router;
