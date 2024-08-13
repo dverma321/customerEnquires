17cdfbfc69c2a0ad2e99fbe0dcc34101a98960f0
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-08-13 16:58:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,12 +36,14 @@ const userRouter = require('./api/User');
 const message_route = require('./api/ContactUs.js');
 const admincontrol_route = require('./api/Admin_fetch_allMessages.js');
 const resolved_route = require("./api/Resolved_ticket_Route.js");
+const image_upload_route = require("./api/ImageUpload_Route.js");
 
 
 app.use('/user', userRouter);
 app.use('/customer_message', message_route);
 app.use('/admincontrol', admincontrol_route);
 app.use('/resolved', resolved_route);
+app.use('/upload-image', image_upload_route);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
