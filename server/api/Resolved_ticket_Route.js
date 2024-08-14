201315c71c6f85f0b78b742e64c1fbc306c66643
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-08-14 10:16:51 +0530

    Update Resolved_ticket_Route.js

--- server/api/Resolved_ticket_Route.js
+++ server/api/Resolved_ticket_Route.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../model/CustomerMessage');
 
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+
+
+// Cloudinary configuration
+
+cloudinary.config({
+    cloud_name: process.env.cloudinary_name,
+    api_key: process.env.cloudinary_apikey,
+    api_secret: process.env.cloudinary_secretkey
+});
+
+
+
 // Route to fetch all resolved messages (closeTicket: true)
 router.get('/resolved_messages', async (req, res) => {
   try {
@@ -19,5 +33,48 @@ router.get('/resolved_messages', async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch resolved messages.' });
   }
 });
+
+// Remove Data permanently from both mongoose and cloudinary
+
+router.delete('/remove_message/:messageId', async (req, res) => {
+  try {
+    const { messageId } = req.params;
+
+    // Find the message to get the public ID of the image
+    const message = await Message.findById(messageId);
+
+    if (!message) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+
+    // Extract the public ID from the image URL
+    const imageUrl = message.imageUrl;
+    if (imageUrl) {
+      // Extract the public ID by removing the base URL and the file extension
+      const publicId = imageUrl
+        .split('/')
+        .slice(-2)
+        .join('/')
+        .split('.')[0]; // 'customer_enquiry/zxnjnjvlxufddrpemt9u'
+
+      // Delete the image from Cloudinary if it exists
+      await cloudinary.uploader.destroy(publicId);
+    }
+
+    // Delete the message from the database
+    const result = await Message.findByIdAndDelete(messageId);
+
+    if (!result) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+
+    res.status(200).json({ message: 'Message removed successfully' });
+  } catch (error) {
+    console.error('Error removing message:', error);
+    res.status(500).json({ error: 'Error removing message' });
+  }
+});
+
+
 
 module.exports = router;
