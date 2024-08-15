8023dd2cd25bb40d270858ca144145cee6b75660
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-08-15 10:20:18 +0530

    Update SendMessage_Image_Route.js

--- server/api/SendMessage_Image_Route.js
+++ server/api/SendMessage_Image_Route.js
@@ -17,14 +17,13 @@ cloudinary.config({
 
 // Use memory storage for uploading files
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage, limits: { fileSize: 2 * 1024 * 1024 } }); // 2MB limit
+const upload = multer({ storage: storage, limits: { fileSize: 5 * 1024 * 1024 } }); // 2MB limit
 
+// routes/send_message_content_route.js
 
 router.post('/send-message-optionalimage', upload.single('file'), async (req, res) => {
     try {
         const { name, email, orderId, platform, content } = req.body;
-
-        // Step 1: Initialize a new message object
 
         const newMessage = new Message({
             name,
@@ -32,28 +31,20 @@ router.post('/send-message-optionalimage', upload.single('file'), async (req, re
             orderId,
             platform,
             content,
-            receiverEmail: 'divyanshuverma36@gmail.com', // Set the fixed receiver email
+            receiverEmail: 'divyanshuverma36@gmail.com',
         });
 
-
-        // Step 2: If a file is uploaded, process and upload it to Cloudinary
         if (req.file) {
-            // Convert the Buffer to base64 string
             const base64Image = req.file.buffer.toString('base64');
-
             const result = await cloudinary.uploader.upload(`data:${req.file.mimetype};base64,${base64Image}`, {
-                folder: "customer_enquiry", // Specify the folder in Cloudinary
-                use_filename: true, // Use the original filename
+                folder: "customer_enquiry",
+                use_filename: true,
             });
-
-            // Attach the Cloudinary image URL to the message object
             newMessage.imageUrl = result.secure_url;
         }
 
-        // Step 3: Save the message to the database
         const savedMessage = await newMessage.save();
 
-        // Step 4: Return the response with the saved message data
         res.status(201).json({
             message: 'Message created and image uploaded to Cloudinary successfully',
             messageData: savedMessage,
@@ -63,6 +54,7 @@ router.post('/send-message-optionalimage', upload.single('file'), async (req, re
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
 
 
 
