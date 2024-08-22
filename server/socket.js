28a45a4c48408de911128dd363613caf40425386
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-08-22 17:38:18 +0530

    Add files via upload

---
+++ server/socket.js
@@ -0,0 +1,36 @@
+const socketIo = require('socket.io');
+const MessageModel = require('./model/CustomerMessage');
+
+let io;
+
+const initSocket = (server) => {
+  io = socketIo(server, {
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      optionsSuccessStatus: 204
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('join', ({ userId }) => {
+      console.log(`User ${userId} connected with socket ID: ${socket.id}`);
+    });      
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+};
+
+const getIo = () => {
+  if (!io) {
+    throw new Error('Socket.io not initialized');
+  }
+  return io;
+};
+
+module.exports = { initSocket, getIo };
