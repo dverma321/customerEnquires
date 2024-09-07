c806e5e5d27c098d9f822af3a7fdea0d66054dc9
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-09-07 17:06:41 +0530

    Add files via upload

---
+++ server/model/Order.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  orderId: { type: String, required: true, unique: true },
+  customerName: { type: String, required: true },
+  orderDate: { type: Date, required: true },  
+  productName : {type: String, require: true},
+  price: {type: Number, required: true},
+  status: { type: String, default: 'Pending' }, 
+  reimburse: { type: Boolean, default: false }, 
+  platform: { type: String, required: true }, 
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
