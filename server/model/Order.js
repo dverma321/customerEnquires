1e8ca95136f8b23c31c2423023800bcf888b602d
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-10-03 10:02:08 +0530

    Update Order.js

--- server/model/Order.js
+++ server/model/Order.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
   orderId: { type: String, required: true, unique: true },
   customerName: { type: String, required: true },
+  customerCity: { type: String },
+  customerState: { type: String, required: true },
   orderDate: { type: Date, required: true },  
   productName : {type: String, require: true},
   price: {type: Number, required: true},
-  status: { type: String, default: 'Pending' }, 
+  status: { type: String, default: 'pending' }, 
   reimburse: { type: Boolean, default: false }, 
   platform: { type: String, required: true }, 
 });
