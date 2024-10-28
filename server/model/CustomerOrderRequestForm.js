23d905594d4727d6750030d52c85c2a1b8495f9c
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-10-28 16:06:19 +0530

    Add files via upload

---
+++ server/model/CustomerOrderRequestForm.js
@@ -0,0 +1,19 @@
+// models/CustomerOrderRequest.js
+const mongoose = require('mongoose');
+
+const CustomerOrderRequestSchema = new mongoose.Schema({
+  orderId: { type: String, required: true, unique: true },
+  customerName: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  orderDate: { type: Date, required: true },
+  productName: { type: String, required: true },
+  isApproved: { type: Boolean, default: false },
+  userId: { // Reference to User model
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'UserData', // Name of the User model
+    required: true 
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('CustomerOrderRequest', CustomerOrderRequestSchema);
