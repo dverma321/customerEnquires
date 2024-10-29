677cc33cdb2efb5a1f8a724ea7f882732fa5dbe4
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-10-29 09:01:12 +0530

    Add files via upload

---
+++ server/api/productData.js
@@ -0,0 +1,78 @@
+// productData.js
+const products = [
+    {
+      productName: 'nfs_2005',
+      images: ['imaage1'],
+      downloadUrl: 'https://mega.nz/folder/0ytSEQCT#V9lYcyeSomM-cq5VqaKE0Q',
+      version: '2005',
+    },
+    {
+      productName: 'nfs_mostwanted_2012',
+      images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+      downloadUrl: 'https://www.mediafire.com/file/ch72gyyo1kston8/Need_for_Speed_-_Most_Wanted-2012.rar/file',
+      version: '2012',
+    },
+    {
+      productName: 'nfs_payback',
+      images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+      downloadUrl: 'https://mega.nz/folder/BYEkkRiC#Ns59_iTJxd6BhMpMfB0FvA',
+      version: '2017',
+    },
+
+    {
+        productName: 'photoshop_cs6',
+        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        downloadUrl: 'https://drive.google.com/drive/folders/1zbzSHbgkRZWyC4RdY032iInwZA-mbudV?usp=sharing',
+        version: 'CS6',  
+    },
+
+    {
+        productName: 'photoshop_cs_2023',
+        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        downloadUrl: 'https://www.mediafire.com/folder/muntdb9p6q4bl/adobe_photoshop_2024',
+        version: 'Adobe photoshop 2023',  
+    },
+
+    {
+        productName: 'photoshop_element_2025',
+        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        downloadUrl: '',
+        version: '',  
+    },
+
+    {
+        productName: 'ease_data_recovery',
+        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        downloadUrl: 'https://drive.google.com/drive/folders/19wE751B1LQk0srhLTgx2NTmYPLP0JsWo?usp=drive_link',
+        version: '',  
+    },
+
+    {
+        productName: 'stellar_data_recovery',
+        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        downloadUrl: 'https://drive.google.com/drive/folders/1LQBhMfl0qU18wic0HOVa5KhN74lTt__z?usp=sharing',
+        version: '',  
+    },
+
+    {
+        productName: 'iobit_advanced_system_care_pro',
+        images: [
+            'https://snowandwhite.neocities.org/images/computer_free_products/advancedsystemcare.jpg', 
+            'https://snowandwhite.neocities.org/images/computer_free_products/advanced_system_care.jpg', 
+            'https://snowandwhite.neocities.org/images/computer_free_products/iobit_advanced_systemcare.jpg'
+                ],
+        downloadUrl: 'https://www.mediafire.com/file/0n7qttc36yq2s6x/IObit_Advanced_SystemCare_Pro_17.3.0.204.rar/file',
+        version: 'iobit advanced system care 17.3.0',  
+    },
+
+    {
+        productName: '',
+        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        downloadUrl: '',
+        version: '',  
+    }
+
+  ];
+  
+  module.exports = products;
+  
