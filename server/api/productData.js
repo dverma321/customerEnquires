65063ce1eafab9867c507a56a55d24a964d64f47
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-11-01 21:22:37 +0530

    Update productData.js

--- server/api/productData.js
+++ server/api/productData.js
@@ -2,13 +2,15 @@
 const products = [
     {
       productName: 'nfs_2005',
-      images: ['imaage1'],
+      images: ['https://m.media-amazon.com/images/I/51RKJkxZK+L._SX342_SY445_.jpg'],
       downloadUrl: 'https://mega.nz/folder/0ytSEQCT#V9lYcyeSomM-cq5VqaKE0Q',
       version: '2005',
     },
     {
       productName: 'nfs_mostwanted_2012',
-      images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+      images: ['https://m.media-amazon.com/images/I/41Z6PDFMxaL.jpg', 
+                
+               ],
       downloadUrl: 'https://www.mediafire.com/file/ch72gyyo1kston8/Need_for_Speed_-_Most_Wanted-2012.rar/file',
       version: '2012',
     },
@@ -28,16 +30,21 @@ const products = [
 
     {
         productName: 'photoshop_cs_2023',
-        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        images: ['https://m.media-amazon.com/images/I/310oNXBcY0L.jpg', 
+                 'https://m.media-amazon.com/images/I/71nnrZmuFTL._SX679_.jpg', 
+                 'https://m.media-amazon.com/images/I/51C25GDwAuL._SX679_.jpg'
+                ],
         downloadUrl: 'https://www.mediafire.com/folder/muntdb9p6q4bl/adobe_photoshop_2024',
         version: 'Adobe photoshop 2023',  
     },
 
     {
         productName: 'photoshop_element_2025',
-        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
-        downloadUrl: '',
-        version: '',  
+        images: ['https://m.media-amazon.com/images/I/41JZYk33sVL._SX679_.jpg',
+                 
+                ],
+        downloadUrl: 'https://www.mediafire.com/folder/br90ywnfqdf0d/Adobe+photoshop+Element+2025',
+        version: 'adobe photoshop element 2025',  
     },
 
     {
@@ -49,7 +56,9 @@ const products = [
 
     {
         productName: 'stellar_data_recovery',
-        images: ['image1.jpg', 'image2.jpg', 'image3.jpg'],
+        images: ['https://m.media-amazon.com/images/I/41ws1Rio7gL._SY300_SX300_QL70_FMwebp_.jpg', 
+                'https://m.media-amazon.com/images/I/51+hM2bqg8L._SX679_.jpg', 
+                'https://m.media-amazon.com/images/I/316B-88f70L.jpg'],
         downloadUrl: 'https://drive.google.com/drive/folders/1LQBhMfl0qU18wic0HOVa5KhN74lTt__z?usp=sharing',
         version: '',  
     },
